de6cd42750755b5779ee5b40834d7b2858d5d95f
Sam Smith <samsmithnz@gmail.com>
2020-05-06 16:42:22 -0400

    Created initial lead time for changes API call and calculation for Azure DevOPs. GitHub is next.

---
+++ DevOpsMetrics/DevOpsMetrics.Service/Controllers/LeadTimeForChangesController.cs
@@ -0,0 +1,35 @@
+﻿using DevOpsMetrics.Service.DataAccess;
+using DevOpsMetrics.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevOpsMetrics.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadTimeForChangesController : ControllerBase
+    {
+        //How will this work?
+        //We will look at pipelines, noting when a pipelines with a branch exist, looking at that lifecycle, and comparing it to commits with that branch. 
+        //This could be complicated by branch deletions - common with Pull Request merges
+
+
+        //[HttpGet("GetAzLeadTimeForChanges")]
+        //public async Task<List<AzureDevOpsBuild>> GetAzLeadTimeForChanges(string patToken, string organization, string project, string AzureDevOpsbranch, string buildId)
+        //{
+        //    AzureDevOpsLeadTimeForChangesDA da = new AzureDevOpsLeadTimeForChangesDA();
+        //    List<AzureDevOpsBuild> leadTimeForChanges = await da.GetDeployments(patToken, organization, project, AzureDevOpsbranch, buildId);
+        //    return deployments;
+        //}
+
+        //[HttpGet("GetGHLeadTimeForChanges")]
+        //public async Task<List<GitHubActionsRun>> GetGHLeadTimeForChanges(string owner, string repo, string GHbranch, string workflowId)
+        //{
+        //    GitHubLeadTimeForChangesDA da = new GitHubLeadTimeForChangesDA();
+        //    List<GitHubActionsRun> leadTimeForChanges = await da.GetDeployments(owner, repo, GHbranch, workflowId);
+        //    return deployments;
+        //}
+
+    }
+}
---
+++ DevOpsMetrics/DevOpsMetrics.Service/Models/PullRequestCommit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOpsMetrics.Service.Models
+{
+    public class PullRequestCommit
+    {
+        public string commitId { get; set; }
+        public Committer committer { get; set; }
+    }
+
+    public class Committer
+    {
+        public string name { get; set; }
+        public DateTime date { get; set; }
+    }
+}
