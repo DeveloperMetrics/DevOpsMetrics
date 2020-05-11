1d526d56542c51608fa6b82d49328c0d3bd4595b
Sam Smith <samsmithnz@gmail.com>
2020-05-11 19:41:26 -0400

    Added two new properties

--- DevOpsMetrics/DevOpsMetrics.Service/Models/Common/DeploymentFrequencyModel.cs
+++ DevOpsMetrics/DevOpsMetrics.Service/Models/Common/DeploymentFrequencyModel.cs
@@ -8,7 +8,44 @@ namespace DevOpsMetrics.Service.Models.Common
         public string DeploymentName { get; set; }
         public bool IsAzureDevOps { get; set; }
         public List<Build> BuildList { get; set; }
-        public float DeploymentsPerDayMetric { get; set; }
+        private float _deploymentsPerDayMetric;
+        public float DeploymentsPerDayMetric
+        {
+            get
+            {
+                return _deploymentsPerDayMetric;
+            }
+            set
+            {
+                _deploymentsPerDayMetric = value;
+                float dailyDeployment = 1f;
+                float weeklyDeployment = 1f / 7f;
+                float monthlyDeployment = 1f / 30f;
+
+                if (value > dailyDeployment) //NOTE: Does not capture on-demand deployments
+                {
+                    DeploymentsToDisplayMetric = value;
+                    DeploymentsToDisplayUnit = "per day";
+                }
+                else if (value <= dailyDeployment && value >= weeklyDeployment)
+                {
+                    DeploymentsToDisplayMetric = value * 7;
+                    DeploymentsToDisplayUnit = "times per week";
+                }
+                else if (value < weeklyDeployment && value >= monthlyDeployment)
+                {
+                    DeploymentsToDisplayMetric = value * 30;
+                    DeploymentsToDisplayUnit = "times per month";
+                }
+                else if (value < monthlyDeployment)
+                {
+                    DeploymentsToDisplayMetric = value * 30;
+                    DeploymentsToDisplayUnit = "times per month";
+                }
+            }
+        }
+        public float DeploymentsToDisplayMetric { get; set; }
+        public string DeploymentsToDisplayUnit { get; set; }
         public string DeploymentsPerDayMetricDescription { get; set; }
     }
 }
