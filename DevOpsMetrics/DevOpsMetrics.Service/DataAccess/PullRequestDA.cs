38c7301603e2968d67bc4f46240b8e12c92d8ae6
Sam Smith <samsmithnz@gmail.com>
2020-05-18 10:59:06 -0400

    Refactored pull request calls to APIs to JToken arrays

--- DevOpsMetrics/DevOpsMetrics.Service/DataAccess/PullRequestDA.cs
+++ DevOpsMetrics/DevOpsMetrics.Service/DataAccess/PullRequestDA.cs
@@ -8,19 +8,25 @@ namespace DevOpsMetrics.Service.DataAccess
 {
     public class PullRequestDA
     {
-        public async Task<List<AzureDevOpsPRCommit>> GetAzureDevOpsPullRequestCommits(string patToken, string organization, string project, string repositoryId, string pullRequestId)
+        public async Task<Newtonsoft.Json.Linq.JArray> GetAzureDevOpsPullRequestCommitsJArray(string patToken, string organization, string project, string repositoryId, string pullRequestId)
         {
+            Newtonsoft.Json.Linq.JArray list = null;
             //https://docs.microsoft.com/en-us/rest/api/azure/devops/git/pull%20request%20commits/get%20pull%20request%20commits?view=azure-devops-rest-5.1
             string url = $"https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullRequests/{pullRequestId}/commits?api-version=5.1";
             string response = await MessageUtility.SendAzureDevOpsMessage(url, patToken);
-            
-            List<AzureDevOpsPRCommit> commits = new List<AzureDevOpsPRCommit>();
             if (string.IsNullOrEmpty(response) == false)
             {
                 dynamic buildListObject = JsonConvert.DeserializeObject(response);
-                Newtonsoft.Json.Linq.JArray value = buildListObject.value;
-                commits = JsonConvert.DeserializeObject<List<AzureDevOpsPRCommit>>(value.ToString());
+                list = buildListObject.value;
             }
+            return list;
+        }
+
+        public async Task<List<AzureDevOpsPRCommit>> GetAzureDevOpsPullRequestCommits(string patToken, string organization, string project, string repositoryId, string pullRequestId)
+        {
+            Newtonsoft.Json.Linq.JArray list = await GetAzureDevOpsPullRequestCommitsJArray(patToken, organization, project, repositoryId, pullRequestId);
+            List<AzureDevOpsPRCommit> commits = JsonConvert.DeserializeObject<List<AzureDevOpsPRCommit>>(list.ToString());
+
             return commits;
         }
 
@@ -30,7 +36,7 @@ namespace DevOpsMetrics.Service.DataAccess
             //GET /repos/:owner/:repo/pulls
             string url = $"https://api.github.com/repos/{owner}/{repo}/pulls?state=all&head={branch}&per_page=100";
             string response = await MessageUtility.SendGitHubMessage(url, clientId, clientSecret);
-            
+
             List<GitHubPR> prs = new List<GitHubPR>();
             if (string.IsNullOrEmpty(response) == false)
             {
@@ -51,20 +57,25 @@ namespace DevOpsMetrics.Service.DataAccess
             return prId;
         }
 
-        public async Task<List<GitHubPRCommit>> GetGitHubPullRequestCommits(string clientId, string clientSecret, string owner, string repo, string pull_number)
+        public async Task<Newtonsoft.Json.Linq.JArray> GetGitHubPullRequestCommitsJArray(string clientId, string clientSecret, string owner, string repo, string pull_number)
         {
+            Newtonsoft.Json.Linq.JArray list = null;
             //https://developer.GitHub.com/v3/pulls/#list-commits-on-a-pull-request
             //GET /repos/:owner/:repo/pulls/:pull_number/commits
             string url = $"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}/commits?per_page=100";
             string response = await MessageUtility.SendGitHubMessage(url, clientId, clientSecret);
-
-            List<GitHubPRCommit> commits = new List<GitHubPRCommit>();
             if (string.IsNullOrEmpty(response) == false)
             {
                 dynamic buildListObject = JsonConvert.DeserializeObject(response);
-                Newtonsoft.Json.Linq.JArray value = buildListObject;
-                commits = JsonConvert.DeserializeObject<List<GitHubPRCommit>>(value.ToString());
+                list = buildListObject;
             }
+            return list;
+        }
+
+        public async Task<List<GitHubPRCommit>> GetGitHubPullRequestCommits(string clientId, string clientSecret, string owner, string repo, string pull_number)
+        {
+            Newtonsoft.Json.Linq.JArray list = await GetGitHubPullRequestCommitsJArray(clientId, clientSecret, owner, repo, pull_number);
+            List<GitHubPRCommit> commits = JsonConvert.DeserializeObject<List<GitHubPRCommit>>(list.ToString());
             return commits;
         }
     }
