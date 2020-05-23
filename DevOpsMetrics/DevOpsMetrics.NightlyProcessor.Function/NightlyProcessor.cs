2c7ac437b9c77d8f996dcccda5ee8c395ec72be6
Sam Smith <samsmithnz@gmail.com>
2020-05-23 09:27:09 -0400

    Added debug statements

--- DevOpsMetrics/DevOpsMetrics.NightlyProcessor.Function/NightlyProcessor.cs
+++ DevOpsMetrics/DevOpsMetrics.NightlyProcessor.Function/NightlyProcessor.cs
@@ -21,47 +21,53 @@ namespace DevOpsMetrics.NightlyProcessor.Function
         [FunctionName("UpdateStorageTables")]
         public static async Task Run([TimerTrigger(everyTwoSeconds)] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
-            try
+            //try
+            //{
+            //if (functionIsRunning == false)
+            //{
+            //    functionIsRunning = true;
+            log.LogInformation($"C# Timer trigger function started at: {DateTime.Now}");
+
+            //Load settings
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            //Get settings
+            ServiceApiClient api = new ServiceApiClient(configuration);
+            List<AzureDevOpsSettings> azSettings = await api.GetAzureDevOpsSettings();
+            List<GitHubSettings> ghSettings = await api.GetGitHubSettings();
+
+            log.LogInformation($"Checking AzureStorageAccountAccessKey environment variable: {configuration["AppSettings:AzureStorageAccountAccessKey"]}");
+
+
+            //Loop through each setting to update the runs, pull requests and pull request commits
+            int numberOfDays = 30;
+            int maxNumberOfItems = 20;
+            int totalResults = 0;
+            foreach (AzureDevOpsSettings item in azSettings)
             {
-                if (functionIsRunning == false)
-                {
-                    functionIsRunning = true;
-                    log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-                    //Load settings
-                    IConfiguration configuration = new ConfigurationBuilder()
-                        .SetBasePath(context.FunctionAppDirectory)
-                        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                        .AddUserSecrets(Assembly.GetExecutingAssembly(), false)
-                        .AddEnvironmentVariables()
-                        .Build();
-
-                    //Get settings
-                    ServiceApiClient api = new ServiceApiClient(configuration);
-                    List<AzureDevOpsSettings> azSettings = await api.GetAzureDevOpsSettings();
-                    List<GitHubSettings> ghSettings = await api.GetGitHubSettings();
-
-                    //Loop through each setting to update the runs, pull requests and pull request commits
-                    int numberOfDays = 30;
-                    int maxNumberOfItems = 20;
-                    int totalResults = 0;
-                    foreach (AzureDevOpsSettings item in azSettings)
-                    {
-                        totalResults += await api.UpdateAzureDevOpsBuilds(item.PatToken, item.Organization, item.Project, item.Branch, item.BuildName, item.BuildId, numberOfDays, maxNumberOfItems);
-                        totalResults += await api.UpdateAzureDevOpsPullRequests(item.PatToken, item.Organization, item.Project, item.Branch, item.BuildName, item.BuildId, numberOfDays, maxNumberOfItems);
-                    }
-                    foreach (GitHubSettings item in ghSettings)
-                    {
-                        totalResults += await api.UpdateGitHubActionRuns(item.ClientId, item.ClientSecret, item.Owner, item.Repo, item.Branch, item.WorkflowName, item.WorkflowId, numberOfDays, maxNumberOfItems);
-                        totalResults += await api.UpdateGitHubActionPullRequests(item.ClientId, item.ClientSecret, item.Owner, item.Repo, item.Branch, item.WorkflowName, item.WorkflowId, numberOfDays, maxNumberOfItems);
-                    }
-                    log.LogInformation($"C# Timer trigger function complete at: {DateTime.Now} after updating {totalResults} records");
-                }
+                log.LogInformation($"Checking item.PatToken variable: {item.PatToken}");
+                totalResults += await api.UpdateAzureDevOpsBuilds(item.PatToken, item.Organization, item.Project, item.Branch, item.BuildName, item.BuildId, numberOfDays, maxNumberOfItems);
+                totalResults += await api.UpdateAzureDevOpsPullRequests(item.PatToken, item.Organization, item.Project, item.Branch, item.BuildName, item.BuildId, numberOfDays, maxNumberOfItems);
             }
-            finally
+            foreach (GitHubSettings item in ghSettings)
             {
-                functionIsRunning = false;
+                log.LogInformation($"Checking item.ClientId variable: {item.ClientId}");
+                log.LogInformation($"Checking item.ClientSecret variable: {item.ClientSecret}");
+                totalResults += await api.UpdateGitHubActionRuns(item.ClientId, item.ClientSecret, item.Owner, item.Repo, item.Branch, item.WorkflowName, item.WorkflowId, numberOfDays, maxNumberOfItems);
+                totalResults += await api.UpdateGitHubActionPullRequests(item.ClientId, item.ClientSecret, item.Owner, item.Repo, item.Branch, item.WorkflowName, item.WorkflowId, numberOfDays, maxNumberOfItems);
             }
+            log.LogInformation($"C# Timer trigger function complete at: {DateTime.Now} after updating {totalResults} records");
+            //    }
+            //}
+            //finally
+            //{
+            //    functionIsRunning = false;
+            //}
         }
 
     }
