e5610fe2edf84bab00ac42c84cf762ec21ae3617
Sam Smith <samsmithnz@gmail.com>
2020-06-06 13:41:57 -0400

    Created ASP.NET Core TypeScript base application

---
+++ DevOpsMetrics/DevOpsMetrics.AzureDevOpsExtension/scripts/app.ts
@@ -0,0 +1,5 @@
+﻿function sayHello() {
+    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
