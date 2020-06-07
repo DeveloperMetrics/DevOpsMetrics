72191daa3821d22b037982ed92d0ceb977f98230
Sam Smith <samsmithnz@gmail.com>
2020-06-07 15:34:50 -0400

    removed JS files generated by TS

--- DevOpsMetrics/DevOpsMetrics.AzureDevOpsExtension/scripts/app.js
+++
@@ -1,6 +0,0 @@
-function sayHello() {
-    const compiler = document.getElementById("compiler").value;
-    const framework = document.getElementById("framework").value;
-    return `Hello from ${compiler} and ${framework}!`;
-}
-//# sourceMappingURL=app.js.map
