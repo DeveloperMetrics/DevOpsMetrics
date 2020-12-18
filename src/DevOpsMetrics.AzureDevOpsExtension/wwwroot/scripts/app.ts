c71705540e5b5717a2c1e0dd342917a2554ac70b
Sam Smith <samsmithnz@gmail.com>
2020-12-18 07:39:39 -0500

    Removed old stale code

--- src/DevOpsMetrics.AzureDevOpsExtension/wwwroot/scripts/app.ts
+++
@@ -1,5 +0,0 @@
-function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello from ${compiler} and ${framework}!`;
-}
