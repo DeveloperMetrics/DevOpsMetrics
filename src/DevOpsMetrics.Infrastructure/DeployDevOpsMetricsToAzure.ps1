4758592b20c9180dcccbbea5f589cf61316fd97e
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-04-16 12:20:38 -0400

    added app insights

--- src/DevOpsMetrics.Infrastructure/DeployDevOpsMetricsToAzure.ps1
+++ src/DevOpsMetrics.Infrastructure/DeployDevOpsMetricsToAzure.ps1
@@ -3,16 +3,19 @@
 cls
 
 $apprefix ="Devops2"
-$region = "East US" #East US
+$region = "EastUS" #East US
 $regionShort = "eu" #East US
 $environment = "Prod"
 $storageSKU = "Standard_LRS"
+$webplanSKU = "S1" #lowest plan that allows slots
 $tags = "" #Space-separated tags: key[=value] [key[=value] ...]. Use "" to clear existing tags.
 
 $resourceGroup = "$apprefix$Environment"
 $apprefixLC = $apprefix.ToLower()
 $environmentLC = $Environment.ToLower()
 
+$webplanName = "$apprefixLC-$environmentLC-$regionShort-plan"
+$appInsightsName = "$apprefixLC-$environmentLC-$regionShort-appinsights"
 $functionName = "$apprefixLC-$environmentLC-$regionShort-function"
 $serviceName = "$apprefixLC-$environmentLC-$regionShort-service"
 $webName = "$apprefixLC-$environmentLC-$regionShort-web"
@@ -22,27 +25,44 @@ if ($storageName.Length -gt 24)
     $storageName = $storageName.subString(0, 24)
 }
 
+Write-Host "Resources to create:"
+$webplanName
+$appInsightsName
 $functionName
 $serviceName
 $webName
 $storageName
-
-#Validate names
-$storageValidationJson = az storage account check-name --name $storageName
-$storageValidation = $storageValidationJson | ConvertFrom-Json    
-if ($storageValidation.nameAvailable -eq $false) 
-{
-    Write-Host "Storage account $storageName not available"
-    break; #stop the script from running
-}                          
+                         
 
 #Create resource group
 az group create --name $resourceGroup --location $region
 
 #storage account
-az storage account create --name $storageName --resource-group $resourceGroup --sku $storageSKU --tags $tags
-#az storage account show --name $storageName --resource-group $resourceGroup
-#az storage account delete --name $storageName --resource-group $resourceGroup
+$createStorage = $false
+$currentStorageJson = az storage account show --name $storageName --resource-group $resourceGroup
+$currentStorage = $currentStorageJson | ConvertFrom-Json 
+if ($currentStorage -eq $null) 
+{ 
+    #If the storage account hasn't already been created, check if it's available to create
+    $storageValidationJson = az storage account check-name --name $storageName
+    $storageValidation = $storageValidationJson | ConvertFrom-Json    
+    if ($storageValidation.nameAvailable -eq $false) 
+    {
+        Write-Host "Storage account $storageName not available"
+        break; #stop the script from running
+    } 
+    else
+    {
+        $createStorage = $true
+    }
+}
+if ($createStorage -eq $true)
+{
+    az storage account create --name $storageName --resource-group $resourceGroup --sku $storageSKU --tags $tags
+    #az storage account show --name $storageName --resource-group $resourceGroup
+    #az storage account delete --name $storageName --resource-group $resourceGroup
+}
+
 
 $connectionStringJson = az storage account show-connection-string --name $storageName --resource-group $resourceGroup
 $connectionString = $connectionStringJson | ConvertFrom-Json
@@ -74,7 +94,12 @@ az storage table create --name $AzureStorageAccountContainerTableLog --connectio
 
 
 #Create a web plan
+az appservice plan create --name $webplanName --resource-group $resourceGroup --location $region --sku $webplanSKU --tags $tags
+#az appservice plan show --name "devopsmetrics-prod-eu-hosting" --resource-group "DevOpsMetrics"
+
 #Create app insights resource
+az monitor app-insights component create --app $appInsightsName --resource-group $resourceGroup --location $region --tags $tags
+az monitor app-insights component delete --app $appInsightsName --resource-group $resourceGroup
 
 #Create a web app for the web api
 #Set configurations in the web app
