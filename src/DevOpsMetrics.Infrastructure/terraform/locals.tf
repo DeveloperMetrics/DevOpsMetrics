7bf8dc3941d61fac4ebb94dcc85211513d33211d
Paul Kelleher <paul.kelleher@contino.io>
2022-05-20 18:02:30 +0100

    lower

--- src/DevOpsMetrics.Infrastructure/terraform/locals.tf
+++ src/DevOpsMetrics.Infrastructure/terraform/locals.tf
@@ -1,8 +1,8 @@
 locals {
   environment        = terraform.workspace
   basename           = "${var.project_name}-${substr(var.location, 0, 3)}-${var.environment}"
-  keyvaultname       = replace(local.basename, "-", "")
-  storageaccountname = replace(local.basename, "-", "")
+  keyvaultname       = lower(replace(local.basename, "-", ""))
+  storageaccountname = lower(replace(local.basename, "-", ""))
 
 }
 
