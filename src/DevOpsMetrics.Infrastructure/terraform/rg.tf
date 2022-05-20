ce2a4c7e43eb2c132b4feed01351908fc01ce81f
Paul Kelleher <paul.kelleher@contino.io>
2022-05-20 19:13:39 +0100

    owner2

--- src/DevOpsMetrics.Infrastructure/terraform/rg.tf
+++ src/DevOpsMetrics.Infrastructure/terraform/rg.tf
@@ -4,8 +4,3 @@ resource "azurerm_resource_group" "application" {
 }
 
 
-resource "azurerm_role_assignment" "owner" {
-  scope                = azurerm_resource_group.application.id
-  role_definition_name = "Owner"
-  principal_id         = "625b66d7-5b11-40fb-99ab-ba303c13ea88"
-}
