93409c4326c94935a90e8d4b517129cd882d7c3d
Paul Kelleher <paul.kelleher@contino.io>
2022-05-20 19:10:30 +0100

    owner

--- src/DevOpsMetrics.Infrastructure/terraform/rg.tf
+++ src/DevOpsMetrics.Infrastructure/terraform/rg.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "application" {
   name     = "${local.basename}-rg"
   location = var.location
 }
+
+
+resource "azurerm_role_assignment" "owner" {
+  scope                = azurerm_resource_group.application.id
+  role_definition_name = "Owner"
+  principal_id         = "625b66d7-5b11-40fb-99ab-ba303c13ea88"
+}
