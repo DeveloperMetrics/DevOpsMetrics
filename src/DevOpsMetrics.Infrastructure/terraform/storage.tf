5899ca440f5534a92edbea08accec1bdbe672a75
Paul Kelleher <paul.kelleher@contino.io>
2022-05-20 18:32:39 +0100

    v2

--- src/DevOpsMetrics.Infrastructure/terraform/storage.tf
+++ src/DevOpsMetrics.Infrastructure/terraform/storage.tf
@@ -26,14 +26,14 @@ resource "azurerm_storage_table" "storage" {
   storage_account_name = azurerm_storage_account.storage.name
 }
 
-resource "azurerm_key_vault_secret" "connection_string " {
+resource "azurerm_key_vault_secret" "connection_string" {
   name         = "storageAccountConnectionString"
   value        = azurerm_storage_account.primary_connection_string
   key_vault_id = azurerm_key_vault.az_key_vault.id
 }
 
 
-resource "azurerm_key_vault_secret" "access_key " {
+resource "azurerm_key_vault_secret" "access_key" {
   name         = "storageAccountAccessKey"
   value        = azurerm_storage_account.primary_access_key
   key_vault_id = azurerm_key_vault.az_key_vault.id
