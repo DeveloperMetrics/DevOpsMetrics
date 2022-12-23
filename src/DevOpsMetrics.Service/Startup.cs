008cee688f31363883a3366e05592fd051889cd0
Rodrigo Moreirao <rmoreirao@microsoft.com>
2022-12-23 15:46:21 +0100

    improvements on pipeline

--- src/DevOpsMetrics.Service/Startup.cs
+++ src/DevOpsMetrics.Service/Startup.cs
@@ -28,6 +28,8 @@ namespace DevOpsMetrics.Service
             services.AddScoped<IAzureTableStorageDA, AzureTableStorageDA>();
 
             services.AddApplicationInsightsTelemetry();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +38,8 @@ namespace DevOpsMetrics.Service
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
